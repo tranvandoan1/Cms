7d46a2af1c7468f48a426738db14d391158d5ad7
ducky173099 <ducky173099@gmail.com>
2022-07-05 11:23:01 +0700

    k

--- src/app/views/layouts/AppLayout.tsx
+++ src/app/views/layouts/AppLayout.tsx
@@ -21,56 +21,20 @@ import HeaderComponent from "../../shared/components/header/HeaderComponent";
 
 const { Header, Sider, Content, Footer } = Layout;
 const { SubMenu } = Menu;
-const items = [
-  {
-    label: <Link to="/login">menu 1</Link>,
-    key: "login",
-  },
-  {
-    label: <Link to="/home">menu 2</Link>,
-    key: "app",
-  },
-  {
-    label: <Link to="/home">menu 3</Link>,
-    key: "alipay",
-  },
-];
+
 
 function AppLayout() {
   const [collapsed, setCollapsed] = useState(false);
-  const [fullName, setFullName] = useState("");
-  const [iconEvent, setIconEvent] = useState("");
-  const [iconParticipant, setIconParticipant] = useState("");
-  const [iconRole, setIconRole] = useState("");
-  const [iconVersion, setIconVersion] = useState("");
   const location = useLocation();
   const { pathname } = location;
   const pathnames = pathname.split("/").filter((item: any) => item);
   const capatilize = (s: any) => s.charAt(0).toUpperCase() + s.slice(1);
-  const [current, setCurrent] = useState("mail");
-
   const currentLocation = window.location.href.split("/")[3];
 
   useEffect(() => {
-    // const obj = JSON.parse(
-    //   localStorage.getItem(INFO_LOCALSTORAGE.USER_LOGGED) || ""
-    // );
-    // setFullName(obj.fullName);
+  
   }, []);
 
-  const toggle = () => {
-    setCollapsed(!collapsed);
-  };
-
-  const logout = () => {
-    localStorage.clear();
-    window.location.href = "/login";
-  };
-
-  const onClick = (e: any) => {
-    console.log("click ", e);
-    setCurrent(e.key);
-  };
 
   return (
     <>
