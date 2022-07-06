3eabcd2517ed6f0bd3832a12585fe4902c1a8367
hieu123 <vn123@gmail.com>
2022-07-06 09:13:34 +0700

    StagePlot

--- src/app/views/Components/Page/ManageStagePlot/EditStagePlot.tsx
+++ src/app/views/Components/Page/ManageStagePlot/EditStagePlot.tsx
@@ -6,37 +6,37 @@ import { get } from "../../../Api/StagePlot";
 import { editStagePlot } from "../../../Slide/StagePlot";
 
 const EditStagePlot: React.FC = () => {
-  const {id} = useParams();
-  const [StagePlot,setStagePlot] = useState<any>([])
+  const { id } = useParams();
+  const [StagePlot, setStagePlot] = useState<any>();
   const dispath: any = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   useEffect(() => {
-    const getStagePlot = async(id:any) =>{
-        try{   
-            const {data} = await get(id)
-            setStagePlot(data)
-        }catch(error){
-            console.log(error);    
-        }
-    }
-    getStagePlot(id)
+    const getStagePlot = async (id: any) => {
+      try {
+        const { data } = await get(id);
+        setStagePlot(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getStagePlot(id);
   }, []);
 
   const onFinish = (values: any) => {
-    const age = values.age
+    const age = values.age;
     const newStagePlot = {
-        ...values,
-        age: +age,
-        id: id
-    }
+      ...values,
+      age: +age,
+      id: id,
+    };
     console.log(newStagePlot);
-    
-    if(age>0  && age < 100){
-        dispath(editStagePlot(newStagePlot))
-        alert("Sửa thành công")
-        navigate("/admin/manage-stage-plot")
-    }else{
-        alert("Tuổi  không được âm!")
+
+    if (age > 0 && age < 100) {
+      dispath(editStagePlot(newStagePlot));
+      alert("Sửa thành công");
+      navigate("/admin/manage-stage-plot");
+    } else {
+      alert("Tuổi  không được âm!");
     }
   };
 
@@ -44,7 +44,7 @@ const EditStagePlot: React.FC = () => {
     console.log("Failed:", errorInfo);
   };
   console.log(StagePlot);
-  
+
   return (
     <div>
       <div
@@ -56,72 +56,85 @@ const EditStagePlot: React.FC = () => {
       >
         <h3>Update Stage Plot</h3>
       </div>
-      <Form
-        name="basic"
-        labelCol={{
-          span: 6,
-        }}
-        wrapperCol={{
-          span: 18,
-        }}
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Tên Stage Plot"
-          name="name"
-          labelAlign="left"
-          rules={[
-            {
-              required: true,
-              message: "Bạn chưa nhập tên!",
-            },
-          ]}
-        >
-          <Input  placeholder="Tên Các Stage Plot " defaultValue={StagePlot?.name} />
-        </Form.Item>
-
-        <Form.Item
-          label="Tuổi"
-          labelAlign="left"
-          name="age"
-          rules={[
-            {
-              required: true,
-              message: "Bạn chưa nhập tuổi!",
-            },
-          ]}
-        >
-          <Input type="number" placeholder="Nhập Tuổi của bạn!"  defaultValue={StagePlot?.age}/>
-        </Form.Item>
-        <Form.Item
-          label="Màu sắc"
-          labelAlign="left"
-          name="color"
-          rules={[
-            {
-              required: true,
-              message: "Bạn chưa chọn màu!",
-            },
-          ]}
-        >
-          <Input type="text" placeholder="chọn màu" defaultValue={StagePlot?.color} />
-        </Form.Item>
-        <Form.Item
+      {StagePlot != undefined ? (
+        <Form
+          name="basic"
+          labelCol={{
+            span: 6,
+          }}
           wrapperCol={{
-            offset: 6,
             span: 18,
           }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
         >
-          <Button type="primary" htmlType="submit">
-            update
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item
+            label="Tên Stage Plot"
+            name="name"
+            labelAlign="left"
+            rules={[
+              {
+                required: true,
+                message: "Bạn chưa nhập tên!",
+              },
+            ]}
+          >
+            <Input
+              placeholder="Tên Các Stage Plot "
+              defaultValue={StagePlot?.name}
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="Tuổi"
+            labelAlign="left"
+            name="age"
+            rules={[
+              {
+                required: true,
+                message: "Bạn chưa nhập tuổi!",
+              },
+            ]}
+          >
+            <Input
+              type="number"
+              placeholder="Nhập Tuổi của bạn!"
+              defaultValue={StagePlot?.age}
+            />
+          </Form.Item>
+          <Form.Item
+            label="Màu sắc"
+            labelAlign="left"
+            name="color"
+            rules={[
+              {
+                required: true,
+                message: "Bạn chưa chọn màu!",
+              },
+            ]}
+          >
+            <Input
+              type="text"
+              placeholder="chọn màu"
+              defaultValue={StagePlot?.color}
+            />
+          </Form.Item>
+          <Form.Item
+            wrapperCol={{
+              offset: 6,
+              span: 18,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              update
+            </Button>
+          </Form.Item>
+        </Form>
+      ) : null}
     </div>
   );
 };
